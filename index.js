2036a510e955dfe0718fff27cae59a4b1b5e4d29
Dane Schneider <dane.schneider@gmail.com>
2017-04-06 17:47:13 -0400

    cross compile golang lib for linux support

--- index.js
+++ index.js
@@ -1,14 +1,46 @@
 var dotenv = require("dotenv").config()
 
-
 if (process.env.ENVKEY){
   var ffi = require("ffi"),
       path = require('path')
 
-  var libPath = path.join(process.cwd(), "node_modules", "envkey", "ext", "envkey"),
+  var platform = process.platform,
+      arch = process.arch,
+      isUnix = platform != "win32",
+      platformParts,
+      lib
+
+  if (!isUnix){
+    throw "Envkey currently only supports linux and OSX"
+  } else if (platform == "darwin"){
+    platformParts = arch == "x64" ? ["darwin-10.6-amd64"] : ["darwin-10.6-386"]
+  } else {
+    if (arch == "x64"){
+      platformParts = ["linux-amd64", "linux-arm64"]
+    } else if (arch == "ia32"){
+      platformParts = ["linux-386"]
+    } else {
+      platformParts = ["linux-arm64", "linux-arm-7", "linux-arm-6", "linux-arm-5"]
+    }
+  }
+
+  for (var i = 0; i < platformParts.length; i++) {
+    var part = platformParts[i],
+        libPath = path.join(process.cwd(), "node_modules", "envkey", "ext", ["envkey", part].join("-"))
+
+    try {
       lib = ffi.Library(libPath, {
         EnvJson: ['string', ['string']]
       })
+      break
+    } catch (e) {
+      continue
+    }
+  }
+
+  if(!lib){
+    throw "There was a problem loading Evkey on your platform"
+  }
 
   try {
     var res = lib.EnvJson(process.env.ENVKEY)
