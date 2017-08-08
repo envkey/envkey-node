98f8fb025fe99a4ecfd69ebbb01076ce99696df9
Dane Schneider <dane.schneider@gmail.com>
2017-08-07 23:37:52 -0400

    Add error case test

--- __tests__/loader_test.js
+++ __tests__/loader_test.js
@@ -13,6 +13,15 @@ test('it should load and decrypt environment via require autoload', () => {
   clearEnv()
 })
 
+test('it should raise an error with an invalid envkey', ()=> {
+  process.env.ENVKEY = "zvHpsWR9Fx44xYP1MM7D-4kfqz28Sr6eiD9G3invalid-localhost:8090"
+  expect(()=> {
+    var loader = require("../loader.js")
+    loader.load()
+  }).toThrow(/Envkey invalid/)
+  clearEnv()
+})
+
 test('it should load and decrypt environment via loader - synchronously', () => {
   process.env.ENVKEY = "zvHpsWR9Fx44xYP1MM7D-4kfqz28Sr6eiD9G3-localhost:8090"
   var loader = require("../loader.js")
@@ -40,3 +49,4 @@ test('it uses whitelist with "permitted" option', () => {
   expect(process.env.GO_TEST_2).toBeUndefined()
   clearEnv()
 })
+
