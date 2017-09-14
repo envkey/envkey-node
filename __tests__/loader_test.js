d3d1b710d24324296071e40080d009f450eeb0e3
Dane Schneider <dane.schneider@gmail.com>
2017-09-14 18:21:44 -0400

    testing more invalid ENVKEY types

--- __tests__/loader_test.js
+++ __tests__/loader_test.js
@@ -1,7 +1,9 @@
 
 const VALID_ENVKEY = "Emzt4BE7C23QtsC7gb1z-3NvfNiG1Boy6XH2o-env-staging.envkey.com",
       INVALID_ENVKEY = "Emzt4BE7C23QtsC7gb1z-3NvfNiG1Boy6XH2oinvalid-env-staging.envkey.com",
-      INVALID_ENVKEY2 = "Emzt4BE7C23QtsC7gb1zinvalid-3NvfNiG1Boy6XH2o-env-staging.envkey.com"
+      INVALID_ENVKEY2 = "Emzt4BE7C23QtsC7gb1zinvalid-3NvfNiG1Boy6XH2o-env-staging.envkey.com",
+      INVALID_ENVKEY3 = "Emzt4BE7C23QtsC7gb1zinvalid-3NvfNiG1Boy6XH2o-localhost:387946",
+      INVALID_ENVKEY4 = "invalid"
 
 function clearEnv(){
   delete process.env.ENVKEY
@@ -17,15 +19,6 @@ test('it should load and decrypt environment via require autoload', () => {
   clearEnv()
 })
 
-test('it should raise an error with an invalid envkey', ()=> {
-  process.env.ENVKEY = INVALID_ENVKEY
-  expect(()=> {
-    var loader = require("../loader.js")
-    loader.load()
-  }).toThrow(/ENVKEY invalid/)
-  clearEnv()
-})
-
 test('it should load and decrypt environment via loader - synchronously', () => {
   process.env.ENVKEY = VALID_ENVKEY
   var loader = require("../loader.js")
@@ -35,7 +28,38 @@ test('it should load and decrypt environment via loader - synchronously', () => 
   clearEnv()
 })
 
-test('it should load and decrypt environment via loader - asynchronously', () => {
+test('it should raise an error with an invalid envkey', ()=> {
+  process.env.ENVKEY = INVALID_ENVKEY
+  expect(()=> {
+    var loader = require("../loader.js")
+    loader.load()
+  }).toThrow(/ENVKEY invalid/)
+  clearEnv()
+
+  process.env.ENVKEY = INVALID_ENVKEY2
+  expect(()=> {
+    var loader = require("../loader.js")
+    loader.load()
+  }).toThrow(/ENVKEY invalid/)
+  clearEnv()
+
+  process.env.ENVKEY = INVALID_ENVKEY3
+  expect(()=> {
+    var loader = require("../loader.js")
+    loader.load()
+  }).toThrow(/ENVKEY invalid/)
+  clearEnv()
+
+  process.env.ENVKEY = INVALID_ENVKEY4
+  expect(()=> {
+    var loader = require("../loader.js")
+    loader.load()
+  }).toThrow(/ENVKEY invalid/)
+  clearEnv()
+})
+
+
+test('it should load and decrypt environment via loader', () => {
   process.env.ENVKEY = VALID_ENVKEY
   var loader = require("../loader.js")
   loader.load(function(){
